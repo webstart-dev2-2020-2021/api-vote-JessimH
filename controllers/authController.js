5128ab9fa48a514db45a2b05d186c155fc2feacb
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-12 12:59:37 +0200

    Signup function, create a newUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,20 @@
+const User = require('../models/user')
+
 module.exports = {
-    signup(req, res) {
-        return res.status(201).json({
-            success: true,
-            user: {
-                id: 1,
-                username: 'SIGNUP USER'
-            }
-        })
+    async signup(req, res) {
+        const { username, password, email } = req.body
+        try {
+            const newUser = new User({ username, password, email })
+            const savedUser = await newUser.save()
+            return res.status(201).send({
+                success: true,
+                _id: newUser._id,
+                username: newUser.username
+            })
+        } catch (error) {
+            console.log(error)
+            return res.status(500).send('Erreur du serveur')
+        }
     },
     signin(req, res) {
         return res.status(200).json({
