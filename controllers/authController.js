d8203f3f9c6941b1a352dcd1707553fca2d7b930
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-05-08 17:22:27 +0200

    add inputs check

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,12 @@ const User = require('../models/user')
 module.exports = {
     async signup(req, res) {
         const { username, password, email } = req.body
+        if (!username || !password || !email) {
+            return res.status(401).json({
+                success: false,
+                errors: "Vous devez remplir toutes les informations."
+            })
+        }
         try {
             const newUser = new User({ username, password, email })
             const savedUser = await newUser.save()
@@ -16,13 +22,25 @@ module.exports = {
             return res.status(500).send('Erreur du serveur')
         }
     },
-    signin(req, res) {
-        return res.status(200).json({
-            success: true,
-            user: {
-                id: 1,
-                username: 'SIGNIN USER'
-            }
-        })
+    async signin(req, res) {
+        if (!req.body.email || !req.body.password) {
+            return res.status(401).json({
+                success: false,
+                errors: "Tous les champs ne sont pas remplis."
+            })
+        }
+        try {
+            const userToSignIn = await User.findOne({ "email": req.body.email, "password": req.body.password });
+            return res.status(200).json({
+                success: true,
+                user: {
+                    name: userToSignIn.name,
+                    _id: userToSignIn._id
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send('Erreur du serveur');
+        }
     }
 }
